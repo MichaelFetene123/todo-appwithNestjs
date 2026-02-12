47095fb9b4424838a93610ed3b8db4e3674b1ba6
MichaelFetene123 <michaelfetene0202@gmail.com>
2026-02-12 17:51:45 +0300

    Add global HTTP exception filter
    Introduce a global HttpExceptionFilter (src/filters/http-exception.filter.ts) to standardize error responses with statusCode, timestamp and path. Register the filter in AppModule via APP_FILTER and update the MongoDB connection string to 'mongodb://localhost/nestjs-todo'. Also tidy imports and keep TimerMiddleware applied to all routes. The filter returns a generic body for 500 errors and includes the exception message for other statuses.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,20 +1,25 @@
-import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { TodosController } from './todos/todos.controller';
-import { TodosService } from './todos/todos.service';
-import { TodosModule } from './todos/todos.module';
+import { Module, MiddlewareConsumer, NestModule } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { APP_FILTER } from '@nestjs/core';
+import { TodosModule } from './todos/todos.module';
 import { TimerMiddleware } from './middlewares/timer.middleware';
+import { HttpExceptionFilter } from './filters/http-exception.filter';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost/nest'), TodosModule],
-  controllers: [AppController, TodosController],
-  providers: [AppService, TodosService],
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost/nestjs-todo'),
+    TodosModule,
+  ],
+  // Note that we add the Exception Filter to the app here:
+  providers: [
+    {
+      provide: APP_FILTER,
+      useClass: HttpExceptionFilter,
+    },
+  ],
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
-    // consumer.apply(TimerMiddleware).forRoutes('*');
     consumer.apply(TimerMiddleware).forRoutes('*');
   }
 }
